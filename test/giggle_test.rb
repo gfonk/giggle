3fe62045e1c8fbbe428559bc84ea99774ef1427c
Gerald Fontejon <gerald.fontejon@gmail.com>
2014-06-02 16:46:56 -0700

    adding simple version test

--- test/giggle_test.rb
+++ test/giggle_test.rb
@@ -2,7 +2,10 @@ require "test_helper"
 
 class GiggleTest < Test::Unit::TestCase
   def test_giggle
-    # assert_equal "0.0.1", Giggle::VERSION
     assert_equal "zizzle", "giggle".giggle
+  end
+
+  def test_giggle_version
+    assert_equal "0.0.2", Giggle::VERSION
   end
 end
